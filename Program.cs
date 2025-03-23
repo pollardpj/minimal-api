bed72138ffa843e12e398ea46fd250bec2325251
Phil Pollard <phil.pollard@curoservices.com>
2025-03-23 10:19:48 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Weather API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather API V1");
+        c.RoutePrefix = string.Empty; // Serve the Swagger UI at the app's root
+    });
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapPost("/weatherupdate/{city}", ([AsParameters] WeatherUpdateRequest request) =>
+{
+    return Results.Ok(request);
+})
+.WithName("UpdateWeather")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+record WeatherUpdateBody
+{
+    public string SomeBody1 { get; init; }
+    public string SomeBody2 { get; init; }
+}
+
+record WeatherUpdateRequest
+{
+    [FromRoute]
+    public string City { get; init; }
+
+    [FromQuery]
+    public int Temperature { get; init; }
+
+    [FromHeader(Name = "Some-Header")]
+    public string SomeHeader { get; init; }
+
+    [FromBody]
+    public WeatherUpdateBody Body { get; init; }
+}
